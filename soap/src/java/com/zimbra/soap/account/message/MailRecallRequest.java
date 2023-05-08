2e3599ee04db79742c0fc91b06f32fb6bc9a9803
Manjeet Kumar <manjeet.behera01@telusinternational.com>
2023-05-08 18:09:02 +0530

    soap api for mailrecall

--- soap/src/java/com/zimbra/soap/account/message/MailRecallRequest.java
+++ soap/src/java/com/zimbra/soap/account/message/MailRecallRequest.java
@@ -1,7 +1,23 @@
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * Zimbra Collaboration Suite Server
+ * Copyright (C) 2023 Synacor, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software Foundation,
+ * version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with this program.
+ * If not, see <https://www.gnu.org/licenses/>.
+ * ***** END LICENSE BLOCK *****
+ */
+
 package com.zimbra.soap.account.message;
 
 import com.zimbra.common.soap.AccountConstants;
-import com.zimbra.soap.type.ZmBoolean;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
@@ -12,14 +28,14 @@ import javax.xml.bind.annotation.XmlRootElement;
 @XmlRootElement(name = AccountConstants.E_MAIL_RECALL_REQUEST)
 public class MailRecallRequest {
     @XmlAttribute(name = AccountConstants.ITEM_ID /* more */, required = true)
-    private int item_id;
+    private int itemId;
 
-    public int getItem_id() {
-        return item_id; 
+    public int getItemId() {
+        return itemId;
     }
 
-    public void setItem_id(int item_id) {
-        this.item_id = item_id;
+    public void setItemId(int itemId) {
+        this.itemId = itemId;
     }
 }
 
